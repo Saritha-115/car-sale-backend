f4bfcc24798386c0965dff6e3425d116bb16aaa4
Saritha Themiyadasa <124487328+Saritha-115@users.noreply.github.com>
2024-09-06 11:18:21 +0530

    Delete sithum-car-sale/sithum-car-sale directory
    deleting the old backcend

--- sithum-car-sale/sithum-car-sale/src/main/java/com/example/sithum_car_sale/reporsitory/UserReporsitory.java
+++
@@ -1,7 +0,0 @@
-package com.example.sithum_car_sale.reporsitory;
-
-import com.example.sithum_car_sale.model.UserModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserReporsitory extends JpaRepository<UserModel,Long> {
-}
--- sithum-car-sale/sithum-car-sale/src/main/java/com/example/sithum_car_sale/reporsitory/VehicleDataReporsitory.java
+++
@@ -1,8 +0,0 @@
-package com.example.sithum_car_sale.reporsitory;
-
-import com.example.sithum_car_sale.model.VehicleDataModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehicleDataReporsitory extends JpaRepository<VehicleDataModel,Long> {
-
-}
