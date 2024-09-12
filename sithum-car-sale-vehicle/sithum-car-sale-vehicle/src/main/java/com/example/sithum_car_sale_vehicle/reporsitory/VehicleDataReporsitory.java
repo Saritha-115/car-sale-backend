7f9da2eb6e3e0639140f8c8db403d24f44300142
Saritha Themiyadasa <124487328+Saritha-115@users.noreply.github.com>
2024-09-12 18:08:11 +0530

    Delete sithum-car-sale-vehicle/sithum-car-sale-vehicle directory
    deleting the vehicle backend

--- sithum-car-sale-vehicle/sithum-car-sale-vehicle/src/main/java/com/example/sithum_car_sale_vehicle/reporsitory/VehicleDataReporsitory.java
+++
@@ -1,7 +0,0 @@
-package com.example.sithum_car_sale_vehicle.reporsitory;
-
-import com.example.sithum_car_sale_vehicle.model.VehicleDataModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehicleDataReporsitory extends JpaRepository<VehicleDataModel,Long> {
-}
