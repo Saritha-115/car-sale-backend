b96fa6fa7f51f1c93af82eb1f139d713d541f5e7
Saritha Themiyadasa <124487328+Saritha-115@users.noreply.github.com>
2024-09-12 18:07:42 +0530

    Delete sithum-car-sale-user/sithum-car-sale-user directory
    deleting the user backend

--- sithum-car-sale-user/sithum-car-sale-user/src/main/java/com/example/sithum_car_sale_user/reporsitory/UserReporsitory.java
+++
@@ -1,7 +0,0 @@
-package com.example.sithum_car_sale_user.reporsitory;
-
-import com.example.sithum_car_sale_user.model.UserModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserReporsitory extends JpaRepository<UserModel,Long> {
-}
