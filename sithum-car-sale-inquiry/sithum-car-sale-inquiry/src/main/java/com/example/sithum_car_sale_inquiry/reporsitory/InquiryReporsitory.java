276c1f0363b753b261b3bf46cf2b9e80340f3219
Saritha Themiyadasa <124487328+Saritha-115@users.noreply.github.com>
2024-09-12 18:07:19 +0530

    Delete sithum-car-sale-inquiry/sithum-car-sale-inquiry directory
    deleting inquiry backend

--- sithum-car-sale-inquiry/sithum-car-sale-inquiry/src/main/java/com/example/sithum_car_sale_inquiry/reporsitory/InquiryReporsitory.java
+++
@@ -1,7 +0,0 @@
-package com.example.sithum_car_sale_inquiry.reporsitory;
-
-import com.example.sithum_car_sale_inquiry.model.InquiryModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InquiryReporsitory extends JpaRepository<InquiryModel,Long> {
-}
